488bf221416590a732ca1da46b57e376ce8c1f92
Sanshiroh SAKAI <sakai.s@ekzm.co.jp>
2023-04-25 17:13:24 +0900

    ページ追加

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,7 @@ type LayoutProps = {
 
 const Layout: FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="min-h-screen bg-zinc-900 pb-5 text-white dark:bg-zinc-100 dark:text-gray-900">
+    <div className="min-h-screen bg-zinc-100 pb-5 text-gray-900 dark:bg-zinc-900 dark:text-white">
       <div className="m-auto w-[1000px]">
         <Header />
         {children}
---
+++ src/pages/mylist/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Heading from '@/components/Heading'
+
+const MylistPage: NextPage = () => {
+  return (
+    <div className="mt-6">
+      <Heading>マイリスト</Heading>
+    </div>
+  )
+}
+
+export default MylistPage
