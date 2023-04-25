665d622d9e1fa6a88f7482b6f4370f08f6c95920
Sanshiroh SAKAI <sakai.s@ekzm.co.jp>
2023-04-25 15:10:34 +0900

    ローディングスタイルを調整

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -7,11 +7,11 @@ type LoadingProps = {
 const Loading: FC<LoadingProps> = ({ isLoading }) => {
   return (
     <div
-      className={`fixed inset-0 z-50 flex h-screen w-screen flex-col items-center justify-center overflow-hidden bg-zinc-900 transition-all ${
+      className={`pointer-events-none fixed inset-0 z-50 flex h-screen w-screen flex-col items-center justify-center overflow-hidden bg-zinc-900 transition-all ${
         isLoading ? 'opacity-100' : 'opacity-0'
       }`}
     >
-      <div className="loader h-14 w-14 rounded-full border-4 border-t-4 border-gray-200 ease-linear"></div>
+      <div className="loader h-20 w-20 rounded-full border-8 border-t-8 border-gray-200 ease-linear"></div>
     </div>
   )
 }
