f406fe889185eafeca32126a16c61402b38e44ec
Sanshiroh SAKAI <sakai.s@ekzm.co.jp>
2023-04-25 17:40:01 +0900

    テーマ切り替え機能を実装

--- src/components/ToggleThemeButton.tsx
+++ src/components/ToggleThemeButton.tsx
@@ -1,10 +1,26 @@
 import type { FC } from 'react'
-import { Sun } from 'react-feather'
+import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
+import { Moon, Sun } from 'react-feather'
 
 const ToggleThemeButton: FC = () => {
+  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
   return (
-    <button className="ml-auto rounded-md p-1">
-      <Sun />
+    <button
+      type="button"
+      className="ml-auto rounded-md p-1"
+      aria-label="テーマ切り替え"
+      onClick={() => {
+        setTheme(theme === 'dark' ? 'light' : 'dark')
+      }}
+    >
+      {mounted && <>{theme === 'dark' ? <Sun /> : <Moon />}</>}
     </button>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes'
 import { RecoilRoot } from 'recoil'
 import Layout from '@/components/Layout'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ThemeProvider attribute="class" defaultTheme="dark">
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
     </RecoilRoot>
   )
 }
