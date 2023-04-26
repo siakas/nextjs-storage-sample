cfcae46b66d1d7f5d6fced30cf22a26ea4a44da7
Sanshiroh SAKAI <sakai.s@ekzm.co.jp>
2023-04-26 16:32:16 +0900

    マイリスト追加ボタンをテーマ切り替えに対応

--- src/components/ToggleMylistButton.tsx
+++ src/components/ToggleMylistButton.tsx
@@ -29,7 +29,7 @@ const ToggleMylistButton: FC<ToggleMylistButtonProps> = ({ movie }) => {
 
   return (
     <button
-      className="absolute bottom-[6px] right-[6px] rounded-full bg-gray-900 bg-opacity-80 p-2 shadow-lg"
+      className="absolute bottom-[6px] right-[6px] rounded-full bg-gray-100 bg-opacity-60 p-2 shadow-lg dark:bg-gray-900 dark:bg-opacity-80"
       onClick={handleClick}
     >
       {isMovieInMylist ? <Check /> : <Plus />}
