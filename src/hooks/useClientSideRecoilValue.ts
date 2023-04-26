23a7a26769c37e018c95c2be03b1cb98c7ba7148
Sanshiroh SAKAI <sakai.s@ekzm.co.jp>
2023-04-26 16:39:06 +0900

    ESLint エラー修正

--- src/hooks/useClientSideRecoilValue.ts
+++ src/hooks/useClientSideRecoilValue.ts
@@ -1,21 +1,11 @@
+import type { RecoilValue } from 'recoil'
 import { useEffect, useState } from 'react'
 import { useRecoilValue } from 'recoil'
 
-// export function useClientSideRecoilValue<T>(
-//   recoilValue: RecoilValue<T>,
-//   defaultValue: T
-// ): T {
-//   const [isClient, setIsClient] = useState(false)
-//   const value = useRecoilValue(recoilValue)
-
-//   useEffect(() => {
-//     setIsClient(true)
-//   }, [])
-
-//   return isClient ? value : defaultValue
-// }
-
-export const useClientSideRecoilValue = (recoilValue, defaultValue) => {
+export function useClientSideRecoilValue<T>(
+  recoilValue: RecoilValue<T>,
+  defaultValue: T
+): T {
   const [isClient, setIsClient] = useState(false)
   const value = useRecoilValue(recoilValue)
 
